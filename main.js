da87c7fe94e7267de8618cf56d0e969685ac84af
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-03 13:46:48 +0100

    add function getWeatherFromCity openweathermap API

---
+++ main.js
@@ -0,0 +1,35 @@
+function createGPSURL(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ef0d45a2a18a8fc1ca9449306cdaae5c`;
+}
+
+function createWeatherURL(dataObj) {
+  return `https://api.openweathermap.org/data/2.5/weather?lat=${dataObj.lat}&lon=${dataObj.lon}&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
+}
+
+async function getGPSdata(url) {
+  const dataPromise = await fetch(url);
+  const data = await dataPromise.json();
+  const cord = data.coord;
+  return ({ lat: cord.lat.toFixed(2), lon: cord.lon.toFixed(2) });
+}
+
+async function getWeatherdata(url) {
+  const weatherPromise = await fetch(url);
+  const weather = await weatherPromise.json();
+  return weather;
+}
+
+async function getWeatherFromCity(city) {
+  console.log('city', city);
+  const gpsURL = createGPSURL(city);
+  console.log('gpsURL', gpsURL);
+  const gpsData = await getGPSdata(gpsURL);
+  console.log('gpsData', gpsData);
+  const weatherURL = createWeatherURL(gpsData);
+  console.log('weatherURL', weatherURL);
+  const weather = await getWeatherdata(weatherURL);
+  console.log('weather', weather);
+  return weather;
+}
+
+getWeatherFromCity('Mannheim').then((resolve) => console.log(resolve));
