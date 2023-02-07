431207ebdbf2f6a0b67b3ccfcda8ac2ce545cda7
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-07 08:57:24 +0100

    change GPS api url to get 5 options

--- weatherAPI.js
+++ weatherAPI.js
@@ -4,11 +4,12 @@ function createWeatherURL(dataObj) {
   return `https://api.openweathermap.org/data/2.5/weather?lat=${dataObj.lat}&lon=${dataObj.lon}&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
 }
 function createGPSURL(city) {
-  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ef0d45a2a18a8fc1ca9449306cdaae5c`;
+  return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
 }
 async function getGPSdata(url) {
   const dataPromise = await fetch(url);
   const data = await dataPromise.json();
+  console.log(data);
   const cord = data.coord;
   return ({ lat: cord.lat.toFixed(2), lon: cord.lon.toFixed(2) });
 }
@@ -31,6 +32,7 @@ function goodData(data) {
 export default async function getWeatherFromCity(city) {
   const gpsURL = createGPSURL(city);
   const gpsData = await getGPSdata(gpsURL);
+  console.log(gpsData);
   const weatherURL = createWeatherURL(gpsData);
   const weather = await getWeatherdata(weatherURL);
   return goodData(weather);
