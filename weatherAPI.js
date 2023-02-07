29084ea367a47435ecfa7e765bc307359e6b8754
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-07 09:16:11 +0100

    add layout to filter gps data for the right city

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,17 +1,16 @@
 import { makeC } from './helper.js';
+import { filterGPSdata } from './createWeather.js';
 
 function createWeatherURL(dataObj) {
   return `https://api.openweathermap.org/data/2.5/weather?lat=${dataObj.lat}&lon=${dataObj.lon}&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
 }
 function createGPSURL(city) {
-  return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
+  return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=100&appid=ef0d45a2a18a8fc1ca9449306cdaae5c`;
 }
 async function getGPSdata(url) {
   const dataPromise = await fetch(url);
   const data = await dataPromise.json();
-  console.log(data);
-  const cord = data.coord;
-  return ({ lat: cord.lat.toFixed(2), lon: cord.lon.toFixed(2) });
+  return data;
 }
 async function getWeatherdata(url) {
   const weatherPromise = await fetch(url);
@@ -32,8 +31,11 @@ function goodData(data) {
 export default async function getWeatherFromCity(city) {
   const gpsURL = createGPSURL(city);
   const gpsData = await getGPSdata(gpsURL);
-  console.log(gpsData);
-  const weatherURL = createWeatherURL(gpsData);
-  const weather = await getWeatherdata(weatherURL);
-  return goodData(weather);
+
+  const goodGpsData = await filterGPSdata(gpsData);
+  console.log(goodGpsData);
+
+  // const weatherURL = createWeatherURL(gpsData);
+  // const weather = await getWeatherdata(weatherURL);
+  // return goodData(weather);
 }
